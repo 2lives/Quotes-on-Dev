361a246178492c7c86a187f7a672f374654e21dd
Brandon Liu <brandon.slipjet@gmail.com>
2018-03-04 13:59:02 -0800

    submit form jquery done

--- js/api.js
+++ js/api.js
@@ -16,6 +16,7 @@ var lastpage = ''
    lastpage = document.URL;
     e.preventDefault();
     $.ajax( {
+      method: 'GET',
       url: 'http://localhost:3000/project5/wp-json/wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       success: function ( data ) {
         var post = data.shift(); // The data is an array of posts. Grab the first one.
@@ -43,5 +44,37 @@ var lastpage = ''
       }//else
     });//popstate
 
+    $('#submission-button').on('click', function(event) {
+    
+      var quoteData = {
+        status: 'pending',
+        title: $('#quote-author').val(),
+        content: $('#quote-content').val(),
+        _qod_quote_source: $('#quote-source').val(),
+        _qod_quote_source_url: $('#quote-source-url').val()
+      }
+
+      event.preventDefault();
+      $.ajax({
+         method: 'POST',
+         url: api_vars.root_url + 'wp/v2/posts/',
+         data: quoteData,
+
+         beforeSend: function(xhr) {
+            xhr.setRequestHeader( 'X-WP-Nonce', api_vars.nonce );
+         },
+
+         success: function() {
+           $('#quote-submission-form').hide('slow');
+           $('.entry-title').after('<p>'+api_vars.success+'</p>');
+         },//success
+
+         error: function() {
+          $('#quote-submission-form').hide('slow');
+          $('.entry-title').after('<p>'+api_vars.failure+'</p>');
+         }//error
+
+      })//ajax
+   });//submit button on click
 
   } );//jquery
